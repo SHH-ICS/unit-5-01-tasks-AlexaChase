bbb1c98974f9bd0bf5287e79720420512c72285c
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-26 14:15:24 +0000

    new changes

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,2 +1,10 @@
-# Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
-# Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
+# Create a program that will accept the two legs of a right-angle triangle, a and b, and
+# display the length of the hypotenuse, c.
+# Remember to use prompts for the input and labels for the output. Use the
+# formula a2 + b2 = c2 to calculate your answer
+import math
+a= float(input("Insert length a:"))
+b= float(input("Insert length b:"))
+Csquared= (a**2) + (b**2)
+c= math.sqrt(Csquared)
+print("The hypotenuse is:", c)
