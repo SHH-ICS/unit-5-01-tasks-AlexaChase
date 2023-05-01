b65bf45534e49bf8c145da2e4184a935e09246fe
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-05-01 14:15:06 +0000

    new changes

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,2 +1,11 @@
-# Create a program that will ask the user an addition question. 
+# Create a program that will ask the user an addition question.
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
+import random
+a= random.randrange(1,100)
+b= random.randrange(1,100)
+print(a, "+", b, "=",)
+userinput= int(input())
+if userinput== (a+b):
+    print("Correct!")
+if userinput!= (a+b):
+    print("The answer was", a + b)
