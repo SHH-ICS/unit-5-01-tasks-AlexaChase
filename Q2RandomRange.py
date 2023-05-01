8ea86f322a7011e7fb4a7dfc821e6d3680f2f6ef
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-05-01 13:53:02 +0000

    new changes

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,11 +1,16 @@
 # Create a program that accepts 2 numbers from the user.
 # Your program will output a random number between the range given by the user.
 import random
+import math
 a=float(input("Input a number: "))
 b=float(input("Input another number: "))
 if a<b:
+    a= math.ceil(a)
+    b= math.floor(b)
     c=int(random.randint(a,b))
     print(c)
 elif a>b:
+    a= math.floor(a)
+    b= math.ceil(b)
     c=int(random.randint(b,a))
     print(c)
